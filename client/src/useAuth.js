fcd9c98a8389ce0e7b138c33fcae0742291c5a3d
(au689313) <kyryliyk@gmail.com>
2025-03-19 09:56:34 +0100

    first_commit

---
+++ client/src/useAuth.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useAuth(code) {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  console.log(refreshToken);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/login", {
+        code,
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
+      });
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = "/";
+        });
+    }, (expiresIn - 60) * 1000); // refresh token 1 minute before expiration
+    return () => clearInterval(interval); // clear timeout when component unmounts
+  }, [refreshToken, expiresIn]); //refresh token if expires
+
+  return accessToken;
+}
