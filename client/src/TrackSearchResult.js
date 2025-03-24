f8b1793940fd36b290b4de6770ac684b33c68eef
(au689313) <kyryliyk@gmail.com>
2025-03-24 15:15:48 +0100

    w

--- client/src/TrackSearchResult.js
+++ client/src/TrackSearchResult.js
@@ -11,7 +11,11 @@ export default function TrackSearchResult({ track, chooseTrack }) {
       style={{ cursor: "pointer" }}
       onClick={handlePlay}
     >
-      <img src={track.albumUrl} style={{ height: "64px", width: "64px" }}></img>
+      <img
+        src={track.albumUrl}
+        alt={`Album cover for ${track.title}`}
+        style={{ height: "64px", width: "64px" }}
+      ></img>
       <div className="ms-3">
         <div>{track.title}</div>
         <div className="text-muted">{track.artist} </div>
