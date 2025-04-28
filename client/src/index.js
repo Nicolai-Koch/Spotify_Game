e540692f131d82538d374880182d817edffb9ae7
nicolaikoch <202207457@post.au.dk>
2025-04-28 12:06:36 +0200

    error

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Import your WebSocket context provider if it's defined
-import { WebSocketProvider } from "./WebSocketContext";  // Make sure this path is correct!
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Render the App wrapped with WebSocketProvider
+// Render the App without WebSocketProvider
 root.render(
-  <WebSocketProvider>
-    <App />
-  </WebSocketProvider>
+  <App />
 );
